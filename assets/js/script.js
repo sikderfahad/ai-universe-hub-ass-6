6d5117669460706041ad2ad540615c9732c0cb59
sikderfahad <sikderfahad003@gmail.com>
2023-04-01 17:59:43 +0600

    Ai accuracy added

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,57 +167,23 @@ function getShowDetails(user) {
                         <!-- Modal Card Right -->
                         <div class="bg-white border border-gray-200 rounded-lg shadow">
                             <div class="  dark:bg-gray-800 dark:border-gray-700">
-                                <div>
+                                <div class="relative">
                                     <img class="rounded-t-lg" src="${user.image_link[0]}" alt="" />
+                                    <p id="accuracy" class="w-fit absolute top-3 right-3 py-2 px-4 rounded-lg text-white text-sm font-medium bg-enter_price">${user.accuracy.score ? user.accuracy.score * 100 + '% Accuracy' : 'No accuray found!'} </p>
                                 </div>
-                                <div class="p-5">
-                                    <a href="#">
-                                        <h5 onmouseenter="textChanger('${user.id}', 'true')" onmouseleave="textChanger('${user.id}')" id="inputText"
-                                            class="mb-2 text-2xl font-semibold tracking-tight text-dark_1">${user.input_output_examples[0].input}</h5>
-                                    </a>
-                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest
-                                        enterprise technology
-                                        acquisitions of 2021 so far, in reverse chronological order.</p>
-                                    <a href="#"
-                                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                        Read more
-                                        <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor"
-                                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                                clip-rule="evenodd"></path>
-                                        </svg>
-                                    </a>
+                                <div class="p-5 text-center">
+                                    <h5 id="inputText"
+                                    class="mb-2 text-2xl font-semibold tracking-tight text-dark_1">${user.input_output_examples ? user.input_output_examples[0].input : 'Can you give any example?'}</h5>
+                                    
+                                    <p class="mb-3 font-medium text-dark_2 ">${user.input_output_examples ? user.input_output_examples[0].output : 'No! Not Yet! Take a break!!!'}</p>
+                                    
                                 </div>
                             </div>
                         </div>
 
 
                     </div>
-    
+
     `
 }
 
-
-
-// Mouse Over text Changer ----> Output
-function textChanger(elementId, hover) {
-    const url = `https://openapi.programming-hero.com/api/ai/tool/${elementId}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => changeText(data.data, hover))
-}
-
-function changeText(data, hover) {
-    const inputText = document.getElementById('inputText')
-
-    if (hover) {
-
-        inputText.innerText = data.input_output_examples[0].output
-        inputText.style.color = '#03A30A'
-    }
-    else {
-        inputText.innerText = data.input_output_examples[0].input
-        inputText.style.color = '#111111'
-    }
-}
